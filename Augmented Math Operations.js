0d993fd941aa53c091dd293f15ed7fb5c74aba7d
sourabh <sourabhchavan20.sc@gmail.com>
2022-04-22 21:05:21 +0530

    Shortcut

---
+++ Augmented Math Operations.js
@@ -0,0 +1,24 @@
+var a = 3;
+var b = 17;
+var c = 12;
+
+/*common method
+
+a = a +12 ;
+b = 9 + b;
+c = c + 7;
+*/
+
+//Shortcut
+
+a += 12;
+b += 9;
+c += 7;
+
+//similarly for substaction
+
+a -= 6;
+b -= 15;
+c -= 11;
+
+console.log(a, b, c);
---
+++ Decimal_numbers.js
@@ -0,0 +1,11 @@
+var ourDecimal = 5.7;
+
+// Multiply Decimals
+
+var product = 1.5 * 2.2;
+console.log(product);
+
+//Divide decimals
+
+var quotient = 4.5 / 2.5;
+console.log(quotient);
