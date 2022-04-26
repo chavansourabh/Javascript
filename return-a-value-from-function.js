0cc7f505414dedf2fcb464ea83958732e0b53a52
sourabh <sourabhchavan20.sc@gmail.com>
2022-04-26 20:53:13 +0530

    about returning value from function

---
+++ return-a-value-from-function.js
@@ -0,0 +1,18 @@
+function minusSeven(num) {
+    return num - 7;
+}
+
+console.log(minusSeven(10));
+
+
+function timesFive(num) {
+    return num * 5;
+}
+console.log(timesFive(10));
+
+
+function returnValue(n) {
+
+    return n * (n / 2);
+}
+console.log(returnValue(7));
---
+++ undefined-value-returned.js
@@ -0,0 +1,14 @@
+//Example
+
+var sum = 0;
+function addThree() {
+    sum = sum + 3;
+}
+
+function addFive() {
+    sum += 5;
+}
+
+console.log(addFive);
+
+//this would be undefined
