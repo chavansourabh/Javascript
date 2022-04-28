fbfc1f1d2198ca4d2e2f8bb1a476f396316b66f3
sourabh <sourabhchavan20.sc@gmail.com>
2022-04-29 00:23:35 +0530

    make any letter uppercase of a string

---
+++ task/capitalize.js
@@ -0,0 +1,41 @@
+function convert(str, pos) {
+    // input = abc , outout = aBc
+    // input = android , output = anDroid
+
+    ///   steps:
+    /*
+    step 1 find the length of string
+    step 2 split the string into array using .split()
+    step 3 find Nth character 
+    step 4  change character using .touppercase()
+    step 5  join the array into string using .join()
+    step 5  return result
+    step 6 print output
+    */
+
+
+    if (pos <= str.length) {
+        var arr = str.split("");
+
+        var newWord;
+        newWord = arr[pos - 1].toUpperCase();
+
+        arr[pos - 1] = newWord;
+
+        var result = arr.join("");
+        return result;
+    }
+    else {
+        return "character not found";
+    }
+
+}
+
+console.log(convert("abc", 2));
+console.log(convert("android", 3));
+
+
+
+
+
+
---
+++ task/passing-marks.js
@@ -0,0 +1,15 @@
+function passingMarks(num) {
+    // var result  = "";
+    if (num >= 40 && num < 55) {
+        return "Third class";
+    } else if (num > 55 && num < 60) {
+        return "Second class";
+    } else if (num > 60 && num < 66) {
+        return "First class";
+    }
+    else {
+        return "fail";
+    }
+}
+
+console.log(passingMarks(-100));
